32029197908b98c29c55e25848b95f3691eae4fb
Massimo1204 <xiangmassimo@gmail.com>
2022-02-24 19:09:11 +0100

    append correction

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 let randomNumber;
+let counter = 2 ;
 const box = document.getElementById('box');
 const boxBox = document.getElementById('my-box-box');
 const button = document.getElementById('button-off');
-let counter = 2 ;
-const newBox = document.createElement('div');
-newBox.classList.add('my-box' , 'd-inline-block');
+
 
 function addBoxes(){
     randomNumber = Math.floor(Math.random() * 9 + 1);
 
     for(let i = 0 ; i <= randomNumber ; i++){
+        const newBox = document.createElement('div');
+        newBox.classList.add('my-box' , 'd-inline-block' , 'm-1');  
         boxBox.append(newBox);
 
         if( (counter % 3) == 0 && (counter % 5) == 0) {
@@ -24,9 +25,10 @@ function addBoxes(){
         else{
             newBox.innerHTML=counter;
         }
-
+        console.log(newBox.innerHTML);
+        counter++;
     } 
-    counter++;
+
 }
 
 button.addEventListener('click' , addBoxes );
